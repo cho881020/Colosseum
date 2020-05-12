5dc9200b9e2ace0bfbea5525c4fec9546602d919
cho881020 <kj_cho@idealidea.co.kr>
2020-05-12 21:35:42 +0900

    로그인 실패시 토스트 처리

--- app/src/main/java/kr/tjeit/colosseum/MainActivity.kt
+++ app/src/main/java/kr/tjeit/colosseum/MainActivity.kt
@@ -2,6 +2,11 @@ package kr.tjeit.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.tjeit.colosseum.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -14,6 +19,38 @@ class MainActivity : BaseActivity() {
 
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+
+            val email = emailEdt.text.toString()
+            val pw = pwEdt.text.toString()
+
+            ServerUtil.postRequestLogin(mContext, email, pw, object  : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+//                    Log.d("로그인응답", json.toString())
+
+                    val code = json.getInt("code")
+
+                    if (code == 200) {
+
+
+
+                    }
+                    else {
+                        val message = json.getString("message")
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+
+                }
+
+            })
+
+
+        }
 
     }
 
