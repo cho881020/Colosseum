30347a826e9f60e20ff9bb0baca87c6dda16796c
cho881020 <kj_cho@idealidea.co.kr>
2020-05-13 22:18:39 +0900

    이메일 중복 검사 기능 완료

--- app/src/main/java/kr/tjeit/colosseum/SignUpActivity.kt
+++ app/src/main/java/kr/tjeit/colosseum/SignUpActivity.kt
@@ -2,12 +2,16 @@ package kr.tjeit.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kr.tjeit.colosseum.utils.ServerUtil
 import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
+
+    var isEmailCheckOk = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,24 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        emailEdt.addTextChangedListener(object  : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                isEmailCheckOk = false
+
+                emailCheckResultTxt.setText(R.string.id_check_message)
+                emailCheckResultTxt.setTextColor(resources.getColor(R.color.darkGray))
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+        })
 
         emailCheckBtn.setOnClickListener {
 
@@ -30,12 +52,16 @@ class SignUpActivity : BaseActivity() {
 
                     if (code == 200) {
 
+                        isEmailCheckOk = true
+
                         runOnUiThread {
                             emailCheckResultTxt.setTextColor(resources.getColor(R.color.azul))
                             emailCheckResultTxt.setText(R.string.id_check_success_message)
                         }
                     }
                     else {
+
+                        isEmailCheckOk = false
                         runOnUiThread {
                             emailCheckResultTxt.setTextColor(resources.getColor(R.color.grapefruit))
                             emailCheckResultTxt.setText(R.string.id_check_fail_message)
