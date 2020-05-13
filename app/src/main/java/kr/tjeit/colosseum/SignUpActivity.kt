a7a389990a4666f50b29f85f0793b7e48e4c4a63
cho881020 <kj_cho@idealidea.co.kr>
2020-05-13 22:13:07 +0900

    중복검사 결과 화면에 반영

--- app/src/main/java/kr/tjeit/colosseum/SignUpActivity.kt
+++ app/src/main/java/kr/tjeit/colosseum/SignUpActivity.kt
@@ -2,6 +2,10 @@ package kr.tjeit.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kr.tjeit.colosseum.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -13,6 +17,37 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        emailCheckBtn.setOnClickListener {
+
+            val inputEmail = emailEdt.text.toString()
+
+            ServerUtil.getRequestEmailDuplCheck(mContext, inputEmail, object  : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+                    Log.d("이메일중복응답", json.toString())
+
+                    val code = json.getInt("code")
+
+                    if (code == 200) {
+
+                        runOnUiThread {
+                            emailCheckResultTxt.setTextColor(resources.getColor(R.color.azul))
+                            emailCheckResultTxt.setText(R.string.id_check_success_message)
+                        }
+                    }
+                    else {
+                        runOnUiThread {
+                            emailCheckResultTxt.setTextColor(resources.getColor(R.color.grapefruit))
+                            emailCheckResultTxt.setText(R.string.id_check_fail_message)
+                        }
+
+                    }
+
+                }
+
+            })
+
+        }
 
     }
 
