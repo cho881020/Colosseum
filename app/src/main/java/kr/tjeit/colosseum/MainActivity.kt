baf30e3697cd842aec3286b44d352e733bc11671
cho881020 <kj_cho@idealidea.co.kr>
2020-05-14 20:08:54 +0900

    프사 선택 => 가져온 프사 뿌려주기  - 이미지 픽 인텐트 활용  - Glide로 uri를 기반으로 이미지 로딩

--- app/src/main/java/kr/tjeit/colosseum/MainActivity.kt
+++ app/src/main/java/kr/tjeit/colosseum/MainActivity.kt
@@ -1,9 +1,17 @@
 package kr.tjeit.colosseum
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    val REQ_FOR_PICK_IMAGE_FROM_GALLERY = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +22,28 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        changeProfileBtn.setOnClickListener {
+            val myIntent = Intent(Intent.ACTION_PICK)
+            myIntent.setType("image/*")
+            myIntent.setType(MediaStore.Images.Media.CONTENT_TYPE)
+            startActivityForResult(myIntent, REQ_FOR_PICK_IMAGE_FROM_GALLERY)
+        }
+
     }
 
     override fun setValues() {
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_FOR_PICK_IMAGE_FROM_GALLERY) {
+            if (resultCode == Activity.RESULT_OK) {
+                Glide.with(mContext).load(data?.data).into(myProfileImg)
+
+            }
+        }
 
     }
 
